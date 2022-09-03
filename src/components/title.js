6d5a25aca9f38fb620c1221f519e9777d67848f5
TonnyKamau <tonnykamau6@gmail.com>
2022-09-03 10:36:28 +0300

    layout

---
+++ src/components/title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title=()=>{
+return (
+  <div className="title">
+    <h1>Firebase React</h1>
+    <h2>photos</h2>
+    <p>look at the amazing save photos</p>
+  </div>  
+)
+}
+export default Title;
